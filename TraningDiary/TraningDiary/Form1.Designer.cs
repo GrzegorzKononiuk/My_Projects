58041230cce577d1f42368c26e6088bc6764a106
Grzegorz Kononiuk <grzegorzkononiuk@gmail.com>
2019-12-29 09:16:12 +0100

    added controls + showDialog

--- TraningDiary/TraningDiary/Form1.Designer.cs
+++ TraningDiary/TraningDiary/Form1.Designer.cs
@@ -28,13 +28,78 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.fbwOpen = new System.Windows.Forms.Button();
+            this.splitOpen = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F);
+            this.label1.Location = new System.Drawing.Point(194, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(405, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CHOOSE YOUR TRAINING MODE";
+            // 
+            // fbwOpen
+            // 
+            this.fbwOpen.Location = new System.Drawing.Point(155, 93);
+            this.fbwOpen.Name = "fbwOpen";
+            this.fbwOpen.Size = new System.Drawing.Size(202, 40);
+            this.fbwOpen.TabIndex = 1;
+            this.fbwOpen.Text = "FBW (FULL BODY WORKOUT)";
+            this.fbwOpen.UseVisualStyleBackColor = true;
+            this.fbwOpen.Click += new System.EventHandler(this.fbwOpen_Click);
+            // 
+            // splitOpen
+            // 
+            this.splitOpen.Location = new System.Drawing.Point(433, 93);
+            this.splitOpen.Name = "splitOpen";
+            this.splitOpen.Size = new System.Drawing.Size(202, 40);
+            this.splitOpen.TabIndex = 2;
+            this.splitOpen.Text = "SPLIT";
+            this.splitOpen.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(38, 166);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(731, 301);
+            this.textBox1.TabIndex = 3;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 544);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.splitOpen);
+            this.Controls.Add(this.fbwOpen);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button fbwOpen;
+        private System.Windows.Forms.Button splitOpen;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
     }
 }
 
--- TraningDiary/TraningDiary/Form1.cs
+++ TraningDiary/TraningDiary/Form1.cs
@@ -7,14 +7,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace TraningDiary
 {
     public partial class Form1 : Form
     {
+        private string name;
         public Form1()
         {
             InitializeComponent();
         }
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+        private void fbwOpen_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                name = openFileDialog1.FileName;
+                textBox1.Clear();
+                textBox1.Text = File.ReadAllText(name);
+            }
+           
+        }
+
+      
     }
 }
