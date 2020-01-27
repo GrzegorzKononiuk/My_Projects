cf738975e8bc56ca918a0ccae8bc11385b159b65
Grzegorz Kononiuk <grzegorzkononiuk@gmail.com>
2020-01-27 18:40:55 +0100

    fsdf

---
+++ TrainingDiary/TrainingDiary/Exercises.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TrainingDiary
+{
+    public enum Exercises
+    {
+        BenchPress = 1,
+        Ohp = 2,
+        Pull_Up = 3
+
+    }
+}
---
+++ TrainingDiary/TrainingDiary/TrainingPlan.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TrainingDiary
+{
+    public class TrainingPlan
+    {
+        Random random = new Random();
+        public Exercises exercises
+        {
+            get
+            {
+                Array values = Enum.GetValues(typeof(Exercises));
+                Random random = new Random();
+                Exercises randomBar = (Exercises)values.GetValue(random.Next(values.Length));
+                return randomBar;
+            }
+
+
+        }
+        public int Series
+        {
+            get
+            {
+                int NumberOfSeries = random.Next(3, 5);
+                return NumberOfSeries;
+            }
+
+        }
+        public int Reps
+        {
+            get
+            {
+                int NumberOfReps = random.Next(4, 12);
+                return NumberOfReps;
+            }
+        }
+        public int Weight
+        {
+            get
+            {
+
+                int Weight = random.Next(50, 80);
+                return Weight;
+            }
+        }
+    }
+}
