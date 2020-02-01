fdca0760e7831e57fbd65f6f5612147374f9972a
Grzegorz Kononiuk <grzegorzkononiuk@gmail.com>
2020-02-01 15:56:22 +0100

    safe pdf from html page too hdd

--- TrainingDiary/TrainingDiary/SafePdf.cs
+++ TrainingDiary/TrainingDiary/SafePdf.cs
@@ -12,35 +12,20 @@ namespace TrainingDiary
     public class SafePdf
     {
         MemoryStream memoryStream = new MemoryStream();
-        PdfPTable table = new PdfPTable(3);
+        TrainingPlan plan = new TrainingPlan();
         public byte[] GeneratePDF()
         {
-            if (!Directory.Exists(@"c:\TRAINING"))
-            {
-                //MessageBox.Show("FOLDER TRAINING HAS CREATED IN C:\\TRAINING, CLICK OPEN TO CHECK YOUR TRAINIG");
-                Directory.CreateDirectory(@"c:\TRAINING");
-
-
-            }
-            else
-            {
-                //MessageBox.Show("TRAINING IT'S ALREADY EXIST, CLICK 'SET TRANING' AND OPEN PDF FILE ");
-                Console.WriteLine("FOLDER ISTNIEJE !!!!!!"); 
-            }
+            FileStream fs = File.Open(@"E:\0001.pdf", FileMode.Create);
             Document doc = new Document(PageSize.A4, 15, 15, 0, 0);
+            PdfWriter.GetInstance(doc, fs);
             doc.SetPageSize(PageSize.A4);
             doc.SetMargins(20f, 20f, 20f, 20f);
-            table.WidthPercentage = 100;
-            table.HorizontalAlignment = Element.ALIGN_LEFT;
-
-            PdfWriter.GetInstance(doc, memoryStream);
             doc.Open();
-            table.SetWidths(new float[] { 20f, 50f, 100f });
-            table.HeaderRows = 2;
-            doc.Add(table);
+            doc.Add(new Paragraph(plan.exercises.ToString()));
+
+            
             doc.Close();
             return memoryStream.ToArray();
         }
-       
     }
 }
