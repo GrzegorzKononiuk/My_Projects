687aa7b00a78742fe2b0d2be1bdf1ef95f61672a
Grzegorz Kononiuk <grzegorzkononiuk@gmail.com>
2020-03-31 16:35:57 +0200

    frontend TextView Buttons controls

--- CheckWeather/obj/Debug/90/android/environment.x86_64.s
+++ CheckWeather/obj/Debug/90/android/environment.x86_64.s
@@ -62,7 +62,7 @@ mono_aot_mode_name:
 	.section	.rodata..L.str.8,"aMS",@progbits,1
 	.type	.L.str.8, @object
 .L.str.8:
-	.asciz	"ab084c03-4ad5-429c-998c-8a64151dad3a"
+	.asciz	"0ebac8ee-72e7-4927-b0c3-cf6874fe9595"
 	.size	.L.str.8, 37
 	.section	.rodata..L.str.9,"aMS",@progbits,1
 	.type	.L.str.9, @object
