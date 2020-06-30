a8a2b3740c31a7eebe169b787312c89d51f9a7d6
Grzegorz Kowal <grzegorzkononiuk@gmail.com>
2020-06-30 10:25:37 +0200

    add Classes Game, GameQueryManager

---
+++ MyGamesLibrary/Game.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+namespace MyGamesLibrary
+{
+    public class Game
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Platform { get; set; }
+        public int Price { get; set; }
+        public int HoursPlayed { get; set; }
+        public BitmapImage Cover { get; set; }
+    }
+}
---
+++ MyGamesLibrary/GameQueryManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyGamesLibrary
+{
+    class GameQueryManager
+    {
+        public static IEnumerable<Game> BuildCatalog()
+        {
+            return new List<Game>
+            {
+                new Game { Name = "Johnny America vs. the Pinko" }
+            };
+
+
+        }
+    }
+}
