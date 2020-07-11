67100f3e2053f547cffe7397f4d8e659bcd6bd82
Grzegorz Kowal <grzegorzkononiuk@gmail.com>
2020-07-11 16:46:30 +0200

    Grid + class SortType

--- MyGamesLibrary/MainWindow.xaml.cs
+++ MyGamesLibrary/MainWindow.xaml.cs
@@ -24,7 +24,5 @@ namespace MyGamesLibrary
         {
             InitializeComponent();
         }
-
-       
     }
 }
---
+++ MyGamesLibrary/SortType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyGamesLibrary
+{
+    // pacz ComicQuery.cs
+    public class SortType
+    {
+        public int LessThan { get; set; }
+    }
+}
