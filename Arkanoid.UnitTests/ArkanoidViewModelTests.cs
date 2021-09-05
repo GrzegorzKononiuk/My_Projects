28b4424cf1661448f333f94a51358c0627f21fd1
GrzegorzKononiuk <52098824+GrzegorzKononiuk@users.noreply.github.com>
2021-09-05 14:36:29 +0200

    instalation mock + simple test of interface IRectangleItem

--- Arkanoid.UnitTests/ArkanoidViewModelTests.cs
+++ Arkanoid.UnitTests/ArkanoidViewModelTests.cs
@@ -1,22 +1,38 @@
-﻿using NUnit.Framework;
+﻿using Arkanoid.Model;
+using Moq;
+using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Arkanoid.UnitTests
 {
-    class ArkanoidViewModelTests
+    public class UnitTests
     {
         [Test]
-        public void Testing()
+        public void TestingMock()
+        {
+
+            var author = new Mock<IRectangleItem>();
+            author.SetupGet(p => p.X).Returns(1);
+            author.SetupGet(p => p.Y).Returns(2);
+
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(1, author.Object.X);
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(2, author.Object.Y);
+        }
+
+
+        [Test]
+        public void TestingNUnit()
         {
             var a = new ViewModel.ArkanoidViewModel();
 
 
             var result = a.Test();
 
-            
-            Assert.AreEqual("Helolo world!!", result);
+
+            NUnit.Framework.Assert.AreEqual("Helolo world!!", result);
         }
 
     }
---
+++ Arkanoid/Model/IRectangleItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arkanoid.Model
+{
+    public interface IRectangleItem
+    {
+        double X { get; set; }
+        double Y { get; set; }
+        double Width { get; set; }
+        double Height { get; set; }
+    }
+}
